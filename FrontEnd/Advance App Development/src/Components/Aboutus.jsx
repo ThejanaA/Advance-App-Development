9349efe07b36cadb21600e65adebacb8136fa1c0
ThejanaA <anandthejashwini17@gmail.com>
2024-03-19 14:26:24 +0530

    Day 5.

--- FrontEnd/Advance App Development/src/Components/Aboutus.jsx
+++ FrontEnd/Advance App Development/src/Components/Aboutus.jsx
@@ -1,29 +1,21 @@
 export default function Aboutus() {
-    return (
-        <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-            <img
-                src="https://www.sinclairgore.co.uk/wp-content/uploads/2021/03/OxfordByNightLight5.jpg"
-                alt=""
-                className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-            />
-            <div
-                className="mx-auto max-w-7xl px-6 lg:px-8"
-                aria-hidden="true"
-            >
-                <div className="mx-auto max-w-2xl lg:mx-0">
-                    <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl"></h2>
-                    <p className="mt-6 text-lg leading-8 text-gray-300">
-                       
-                    </p>
-                </div>
-                <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-                    <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-                    </div>
-                    <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-                        {/* Stats */}
-                    </dl>
-                </div>
-            </div>
+  return (
+    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+      <img
+        src="https://www.sinclairgore.co.uk/wp-content/uploads/2021/03/OxfordByNightLight5.jpg"
+        alt=""
+        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+      />
+      <div className="mx-auto max-w-7xl px-6 lg:px-8" aria-hidden="true">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl"></h2>
+          <p className="mt-6 text-lg leading-8 text-gray-300"></p>
         </div>
-    )
+        <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10"></div>
+          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4"></dl>
+        </div>
+      </div>
+    </div>
+  );
 }
